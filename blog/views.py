52c0df32b662136312532c8fc361b9dcd208159b
unknown <edgarvardanyan1999@gmail.com>
2020-05-28 19:38:52 +0400

    'post_crud_views'

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,64 @@
-from django.shortcuts import render
+from django.views.generic import (TemplateView, ListView,
+                                  DetailView, CreateView,
+                                  UpdateView, DeleteView)
+from django.contrib.auth.mixins import LoginRequiredMixin
+from blog.models import Post, Comment
+from django.utils import timezone
+from blog.forms import PostForm, CommentForm
+from django.urls import reverse_lazy
+
 
 # Create your views here.
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+
+class PostListView(ListView):
+    model = Post
+
+    def get_queryset(self):
+        # fieldname__lte: field value must be less than or equal to
+        # order_by('-published_date'): the - sign is for the decreading order
+        return Post.objects.filter(
+            published_date__lte=timezone.now()).order_by('-published_date')
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+# The user most be logged in for seeing some views
+# In this cases I inherit from LoginRequiredMixin
+class CreatePostView(LoginRequiredMixin, CreateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+
+    form_class = PostForm
+    model = Post
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    # Same features as the CreatePostView
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+
+    form_class = PostForm
+    model = Post
+
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    # using reverse_lazy so that url is not called before deleting
+    success_url = reverse_lazy('post_list')
+
+
+class DraftListView(LoginRequiredMixin, ListView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_list.html'
+    model = Post
+
+    def get_queryset(self):
+        # only show unpublished posts
+        return Post.objects.filter(
+            published_date__isnull=True).order_by('-created_date')
+
