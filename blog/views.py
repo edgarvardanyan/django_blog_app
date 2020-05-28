250ea8c8b6547685694b3cb42f22983d85c45a77
unknown <edgarvardanyan1999@gmail.com>
2020-05-28 20:47:00 +0400

    comment+publish views

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,8 @@ from blog.models import Post, Comment
 from django.utils import timezone
 from blog.forms import PostForm, CommentForm
 from django.urls import reverse_lazy
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -62,3 +64,58 @@ class DraftListView(LoginRequiredMixin, ListView):
         return Post.objects.filter(
             published_date__isnull=True).order_by('-created_date')
 
+
+@login_required
+def post_publish(request, pk):
+    # get the post, publish, and go to its detail page
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+
+    return redirect('post_detail', pk=pk)
+
+######################################
+#       the views for comments       #
+######################################
+
+
+@login_required
+def add_comment_to_post(request, pk):
+    # get the post or return Error 404
+    post = get_object_or_404(Post, pk=pk)
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            # get the comment, set the post and save it
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        # set an empty form id method is not POST
+        form = CommentForm()
+
+    return render(request, 'blog/comment_form.html', context={'form': form})
+
+
+@login_required
+def comment_approve(request, pk):
+    # get the comment by its primary key or Error 404
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+
+    # redirect to the corresponfing post after approval
+    return redirect('post_detail', pk=comment.post.pk)
+
+
+@login_required
+def comment_remove(request, pk):
+    # get the comment by its primary key or Error 404
+    comment = get_object_or_404(Comment, pk=pk)
+
+    # get the post pk before deleting the comment
+    post_pk = comment.post.pk
+    comment.delete()
+
+    # go to the post page
+    return redirect('post_detail', pk=post_pk)
